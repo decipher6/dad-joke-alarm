a05efb5c33f0aeebd734244af88e59e42e4ada72
Yaghyesh Ghimire <53189528+decipher6@users.noreply.github.com>
2024-11-06 16:08:19 -0500

    Update alarmclock.js

--- src/components/alarmclock.js
+++ src/components/alarmclock.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import '/Users/yaghyesh/coolapp/src/AlarmClock.css';
+import './AlarmClock.css';
 
 const AlarmClock = () => {
   const [currentTime, setCurrentTime] = useState(new Date());
